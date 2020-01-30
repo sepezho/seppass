cdc22fe36de8be19f300c9ca9b3b58ee1e15aa2e
sepezho <sepezho@gmail.com>
2020-01-30 18:52:57 +0300

    Начал делать начальное меню.

--- key.py
+++ key.py
@@ -1,32 +1,45 @@
+import os
 import gnupg
-from pprint import pprint
+
 gpg = gnupg.GPG(gnupghome='/home/sepezho/.gnupg/')
 
-# key = 'F308143BC139F61C06D060412C4577FE97C3E455'
-# ascii_armored_public_keys = gpg.export_keys(key)
-# ascii_armored_private_keys = gpg.export_keys(key, True)
-# with open('mykeyfile.asc', 'w') as f:
+file_name = 'keyA'
+passphrase = 'myasdasdsae'
+name_real='seasdpezho'
+input_data = gpg.gen_key_input(
+    passphrase=passphrase,
+	name_real= name_real)
+key = gpg.gen_key(input_data)
+key = key.fingerprint
+
+print('KEY CREATED: '+key)
+
+unencrypted_string = 'Who are you? How did you get in my house?'
+encrypted_data = gpg.encrypt(unencrypted_string, name_real)
+encrypted_string = str(encrypted_data)
+decrypted_data = gpg.decrypt(encrypted_string, passphrase=passphrase)
+
+print ('unencrypted_string: '+ unencrypted_string)
+print ('encrypted_string: '+ encrypted_string)
+print ('decrypted_data: '+ str(decrypted_data))
+
+
+# -----------------------------
+# ascii_armored_public_keys = gpg.export_keys(key, False)
+# ascii_armored_private_keys = gpg.export_keys(key, True, passphrase=passphrase)
+# with open(file_name+'.asc', 'w') as f:
 #     f.write(ascii_armored_public_keys)
 #     f.write(ascii_armored_private_keys)
 
+# print('KEY EXPORTED!')
 
-# key_data = open('pubkey.asc').read()
-# import_result = gpg.import_keys(key_data)
-# pprint(import_result.results)
+# gpg.delete_keys(key, True, passphrase=passphrase)
+# gpg.delete_keys(key)
 
-# unencrypted_string = 'Who are you? How did you get in my house?'
-# encrypted_data = gpg.encrypt(unencrypted_string, 'sepezho@gmail.com')
-# encrypted_string = str(encrypted_data)
+# print('KEY DELETED!')
 
+# key_data = open(file_name+'.asc').read()
+# gpg.import_keys(key_data)
 
-unencrypted_string = 'Who are you? How did you get in my house?'
-encrypted_data = gpg.encrypt(unencrypted_string, 'sepezho@gmail.com')
-encrypted_string = str(encrypted_data)
-decrypted_data = gpg.decrypt(encrypted_string, passphrase='EykNOfw5rkC{')
-
-print ('status: '+ encrypted_data.status)
-print ('stderr: '+ encrypted_data.stderr)
-print ('unencrypted_string: '+ unencrypted_string)
-print ('encrypted_string: '+ encrypted_string)
-print ('decrypted_data: '+  str(decrypted_data))
-
+# print('KEY IMPORTED!')
+# ---------------------------
--- modules/start.py
+++ modules/start.py
@@ -1,8 +1,7 @@
 import os
+import gnupg
 import sqlite3
-
 from telebot import types
-from cryptography.fernet import Fernet
 
 def start_main(message, bot_old):
 	global bot
@@ -22,7 +21,6 @@ def start_response(message):
 		bot.send_message(message.chat.id, 'Непонел.', reply_markup = types.ReplyKeyboardRemove(selective=False))
 
 def login(message):
-
 	conn = sqlite3.connect('DataBase.db', check_same_thread=False)
 	c = conn.cursor()
 	query = "SELECT COUNT(*) FROM Users WHERE Username = '" + message.chat.username + "'"
@@ -35,27 +33,80 @@ def login(message):
 		bot.register_next_step_handler(msg, login_response)
 	else:
 		bot.send_message(message.chat.id, 'Ваш телеграм уже связан с вашим акк для этого бота.\n\nВойдите в акк, отправив снова /start', reply_markup = types.ReplyKeyboardRemove(selective=False))
-		return
+def first_setting(message):
+	print(first_setting)
 
 def login_response(message):
-	cipher_key = Fernet.generate_key()
-	cipher = Fernet(cipher_key)
+	# gpg = gnupg.GPG(gnupghome='/home/sepezho/.gnupg/')
+	# input_data = gpg.gen_key_input(
+	#     passphrase=message.text,
+	# 	name_real=message.chat.username)
+	# key = gpg.gen_key(input_data)
+	# key = key.fingerprint
+	key = 'key.fingerprint'
 
-	# passw = message.text.decode("utf-8")
-	passw_ascii=message.text.encode("utf-8")
-
-	encrypted_pass = cipher.encrypt(passw_ascii)
 
 	conn = sqlite3.connect('DataBase.db', check_same_thread=False)
 	c = conn.cursor()
-	query = "INSERT INTO Users (Username, Password, Settings) VALUES ('"+ message.chat.username +"', '"+encrypted_pass.decode('utf-8"')+"', 'obj')"
+	query = "INSERT INTO Users (Username, Password, Settings) VALUES ('"+ message.chat.username +"', '"+key+"', 'obj')"
 	c.execute(query)
 	conn.commit()
 	conn.close()
+	# os.mkdir('/home/sepezho/Documents/seppass/Users_folder/' + message.chat.username)
+	bot.send_message(message.chat.id, 'Пароль зашифрован и сохранен в БД. Так же создана папка, где ваш никнейм выступил в качестве названия.\n\nПароль:\n'+ message.text +'\n\nЭто ваш отпечаток ключа:\n'+ key)
 
-	# os.mkdir('/home/sepezho/Documents/teleBot_seppass/Users_folders/' + message.chat.username)
+	bot.send_message(message.chat.id, 'Перейдем к настройкам.')
+	settings_main(message)
 
-	bot.send_message(message.chat.id, 'Пароль зашифрован и сохранен в БД. Так же создана папка, где ваш никнейм выступил в качестве названия.\n\nПароль:\n'+ encrypted_pass.decode('utf-8"')+'\n\nЭто ваш крипто-ключ:\n'+ cipher_key.decode('utf-8"') + '\n\nСохраните его где-нибудь.')
+
+def settings_main(message):
+	markup = types.InlineKeyboardMarkup()
+	switch_button1 = types.InlineKeyboardButton(text='-', callback_data = '1')
+	markup.add(switch_button1)
+	switch_button2 = types.InlineKeyboardButton(text='2', callback_data = '2')
+	markup.add(switch_button2)
+	switch_button3 = types.InlineKeyboardButton(text='3', callback_data = '3')
+	markup.add(switch_button3)
+	
+	# global first_setting
+	first_setting = bot.send_message(message.chat.id, "Настройка №1", reply_markup = markup)
+	bot.register_next_step_handler(first_setting, sec_setting_list)
+
+
+@bot.callback_query_handler()
+def sec_setting_list(message):
+	print(message.data)
+# def sec_setting_list(message):
+# 	markup = types.InlineKeyboardMarkup()
+# 	switch_button1 = types.InlineKeyboardButton(text='2', callback_data = '1')
+# 	markup.add(switch_button1)
+# 	switch_button2 = types.InlineKeyboardButton(text='2', callback_data = '2')
+# 	markup.add(switch_button2)
+# 	switch_button3 = types.InlineKeyboardButton(text='2', callback_data = '3')
+# 	markup.add(switch_button3)
+# 	switch_button3 = types.InlineKeyboardButton(text='back', callback_data = 'back')
+# 	markup.add(switch_button3)
+# 	sec_setting = bot.send_message(message.chat.id, "Настройка №2", reply_markup = markup)
+
+# 	markup = types.InlineKeyboardMarkup()
+# 	switch_button1 = types.InlineKeyboardButton(text='3', callback_data = '1')
+# 	markup.add(switch_button1)
+# 	switch_button2 = types.InlineKeyboardButton(text='3', callback_data = '2')
+# 	markup.add(switch_button2)
+# 	switch_button3 = types.InlineKeyboardButton(text='3', callback_data = '3')
+# 	markup.add(switch_button3)
+# 	switch_button_back = types.InlineKeyboardButton(text='back', callback_data = 'back')
+# 	markup.add(switch_button_back)
+# 	third_setting = bot.send_message(message.chat.id, "Настройка №3", reply_markup = markup)
+	
+# 	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+# 	markup.add('Да', 'Нет')
+# 	end_settings = bot.send_message(message.chat.id, "Вы закночили?", reply_markup = markup)
+	# bot.register_next_step_handler(end_settings, finish_reg)
+
+# def finish_reg(message):
+	# print(first_setting)
+
 
 def sign(message):
 	bot.send_message(message.chat.id, 'Вход пожилой!', reply_markup = types.ReplyKeyboardRemove(selective=False))
